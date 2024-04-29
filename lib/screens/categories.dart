582daa47574c6900a2bbeaabe21200fd73a8c42c
Mohammed Afthab <afthab2282@gmail.com>
2024-04-29 21:13:48 +0530

    TabBar and Favourites screen added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
-
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavourite,
+  });
+  final void Function(Meal meal) onToggleFavourite;
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
         .where((meal) => meal.categories.contains(category.id))
@@ -14,7 +18,8 @@ class CategoriesScreen extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) =>  MealsScreen(title: category.title, meals: filteredMeals),
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals ,onToggleFavourite: onToggleFavourite,),
       ),
     );
   }
@@ -23,29 +28,24 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your Category'),
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          //availableCategories.map((category) => CategoryGridItem(category : category)).toList();
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context ,  category);
-              },
-            )
-        ],
-      ),
+      children: [
+        //availableCategories.map((category) => CategoryGridItem(category : category)).toList();
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
     );
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -5,14 +5,24 @@ class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
+    required this.onToggleFavourite,
   });
 
   final Meal meal;
+  final void Function (Meal meal) onToggleFavourite;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: (){
+              onToggleFavourite(meal);
+            },
+            icon: const Icon(Icons.star),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -27,10 +37,11 @@ class MealDetailsScreen extends StatelessWidget {
             Text(
               'Ingredients',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
                   ),
             ),
-            const SizedBox(height:16),
+            const SizedBox(height: 16),
             for (final ingredient in meal.ingredients)
               Text(
                 ingredient,
@@ -38,17 +49,21 @@ class MealDetailsScreen extends StatelessWidget {
                       color: Theme.of(context).colorScheme.onBackground,
                     ),
               ),
-              const SizedBox(height: 24),
-               Text(
+            const SizedBox(height: 24),
+            Text(
               'Steps',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
                   ),
             ),
-            const SizedBox(height:16),
+            const SizedBox(height: 16),
             for (final step in meal.steps)
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12 , vertical: 8,),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
                 child: Text(
                   step,
                   textAlign: TextAlign.center,
